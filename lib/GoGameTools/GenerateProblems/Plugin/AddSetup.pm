2db83ea6d55d73cec993f3230a56aa6fa8f5af01
Marcel Gruenauer <bigghosthead@gmail.com>
2019-11-25 15:13:28 +0100

    {{ add_setup }}: add {{ user_is_guided }}

--- lib/GoGameTools/GenerateProblems/Plugin/AddSetup.pm
+++ lib/GoGameTools/GenerateProblems/Plugin/AddSetup.pm
@@ -1,10 +1,21 @@
 package GoGameTools::GenerateProblems::Plugin::AddSetup;
 use GoGameTools::features;
 use GoGameTools::Node;
+use GoGameTools::Log;
 use GoGameTools::Class;
 
 sub handles_directive ($self, %args) {
     return $args{directive} eq 'add_setup';
+}
+
+# The {{ add_setup }} directive guides the user to choose a move
+# depending on the presence of the added stones. So here we don't want
+# the guide circle to appear.
+sub handle_higher_level_directive ($self, %args) {
+    my $parent_node = $args{traversal_context}->get_parent_for_node($args{node});
+    if (defined($parent_node) && $parent_node->directives->{add_setup}) {
+        $args{node}->directives->{user_is_guided} = 1;
+    }
 }
 
 sub handle_cloned_node_for_problem ($self, %args) {
