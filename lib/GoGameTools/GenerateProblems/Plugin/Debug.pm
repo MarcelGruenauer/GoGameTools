50bfcf6d59deb95a24fbfe33a492099f34b79e6c
Marcel Gruenauer <bigghosthead@gmail.com>
2020-05-21 16:13:02 +0200

    generate problems: more fine-grained debugging
    When writing debug information to a generated problem's GC[], it was
    only possible to write all the metadata or none. Now the user can
    specify whether or not to write tags, refs and the tree location (file +
    index).
    
    The reason for this change is that I have files with long paths that
    squashed the Go board. But these files are for books, so they also have
    refs; in that case it's sufficient to display the refs.

--- lib/GoGameTools/GenerateProblems/Plugin/Debug.pm
+++ lib/GoGameTools/GenerateProblems/Plugin/Debug.pm
@@ -4,22 +4,21 @@ use GoGameTools::Assemble;
 use GoGameTools::Class;
 
 # If requested, write the problem's metadata to the generated problem's root
-# node's GC[] property with location, tags and refs. Do this after assembling
-# so the comment doesn't interfere with assembling. The viewer can then choose
-# to display this data.
+# node's GC[] property with location, tags and/or refs. Do this after
+# assembling so the comment doesn't interfere with assembling. The viewer can
+# then choose to display this data.
 sub finalize_problem_2 ($self, %args) {
-    return unless $args{generator}->should_comment_metadata;
-    my $game_info_node = $args{problem}->tree->get_node(0);
-    my %metadata       = $args{problem}->tree->metadata->%*;
-    $game_info_node->add(
-        GC => sprintf(
-            "file %s index %s\n\n%s",
-            $metadata{filename},
-            $metadata{index},
-            join("\n",
-                (map { "#$_" } $metadata{tags}->@*),
-                (map { "\@$_" } $metadata{refs}->@*))
-        )
-    );
+    my @debug;
+    my %metadata = $args{problem}->tree->metadata->%*;
+    if ($args{generator}->should_comment_location) {
+        push @debug, sprintf 'file %s index %s', $metadata{filename}, $metadata{index};
+    }
+    if ($args{generator}->should_comment_tags) {
+        push @debug, join "\n", map { "#$_" } $metadata{tags}->@*;
+    }
+    if ($args{generator}->should_comment_refs) {
+        push @debug, join "\n", map { "\@$_" } $metadata{refs}->@*;
+    }
+    $args{problem}->tree->get_node(0)->add(GC => join "\n\n", @debug) if @debug;
 }
 1;
