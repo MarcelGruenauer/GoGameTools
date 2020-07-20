2a7feed1e722a3bb2f0603be0460512ffc48ab04
Marcel Gruenauer <bigghosthead@gmail.com>
2020-07-20 10:49:50 +0200

    site gen: topics also support 'with_tag' etc., like subsets
    This is in addition to filter queries. Most topics don't need expensive
    filters; they can just grep based on the 'with_tag', 'without_tag',
    'with_ref' and 'without_ref' spec, just like subsets; it is faster.

--- lib/GoGameTools/Porcelain/Subsets.pm
+++ lib/GoGameTools/Porcelain/Subsets.pm
@@ -11,19 +11,29 @@ sub import {
 # Given a subset definition and a problem collection, return the subset of
 # problems that correspond to the subset definition.
 sub get_problems_for_subset ($subset, $problems) {
-    my $subset_query =
-      join ' and ' =>
-      (defined $subset->{with_ref}    ? ('@' . $subset->{with_ref})        : ()),
-      (defined $subset->{without_ref} ? ('not @' . $subset->{without_ref}) : ()),
-      (defined $subset->{with_tag}    ? ('#' . $subset->{with_tag})        : ()),
-      (defined $subset->{without_tag} ? ('not #' . $subset->{without_tag}) : ());
-    if (length $subset_query) {
-        my $expr = parse_filter_query($subset_query);
-        return [ grep { eval_query(expr => $expr, vars => $_->{vars}) } $problems->@* ];
-    } else {
+    my @remaining = $problems->@*;
 
-        # this subset has no restrictions; used for 'All' quasi-subsets
-        return $problems;
+    # gradually restrict the resulting list
+    if (defined(my $tag = $subset->{with_tag})) {
+        @remaining = grep { exists $_->{vars}{tags}{$tag} } @remaining;
     }
+    if (defined(my $tag = $subset->{without_tag})) {
+        @remaining = grep { !exists $_->{vars}{tags}{$tag} } @remaining;
+    }
+    if (defined(my $ref = $subset->{with_ref})) {
+        @remaining = grep { exists $_->{vars}{refs}{$ref} } @remaining;
+    }
+    if (defined(my $ref = $subset->{without_ref})) {
+        @remaining = grep { !exists $_->{vars}{refs}{$ref} } @remaining;
+    }
+
+    # if you have a more complex query you can use 'filter'
+    if (defined(my $filter = $subset->{filter})) {
+        my $expr = parse_filter_query($filter)
+          // die "can't parse filter query $filter\n";
+        @remaining =
+          grep { eval_query(expr => $expr, vars => $_->{vars}) } @remaining;
+    }
+    return \@remaining;
 }
 1;
