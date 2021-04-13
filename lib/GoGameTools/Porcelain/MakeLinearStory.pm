3a54509719383b7a812064ce73a00a2da6f602fe
Marcel Gruenauer <bigghosthead@gmail.com>
2021-04-13 18:09:44 +0200

    refactored making linear stories
    There JSON file where the story is specified can now contain different
    types of instructions.
    
    The "path" instruction is how it worked up to now. You specify a "from"
    and a "to" node and this node list is added to the story.
    
    The new "node" instruction just adds a single node to the story.
    
    The new "traverse" instruction goes to the "from" node and does a
    depth-first traversal, adding a node list for each variation.

--- lib/GoGameTools/Porcelain/MakeLinearStory.pm
+++ lib/GoGameTools/Porcelain/MakeLinearStory.pm
@@ -27,8 +27,8 @@ sub run ($self) {
                 $_->traverse(
                     sub ($node, $context) {
                         track_board_in_traversal_for_node($node, $context);
-                        our %parent_of_node;
-                        parent_of_node($node, $context->get_parent_for_node($node));
+                        parent_for_node($node, $context->get_parent_for_node($node));
+                        tree_for_node($node, $_);
                         if (defined(my $name = $node->directives->{name})) {
                             node_for_name($_->metadata->{filename}, $_->metadata->{index}, $name, $node);
                         }
@@ -40,48 +40,21 @@ sub run ($self) {
     );
 }
 
+# This pipe segment doesn't actually use the collection, but builds up the
+# resulting tree from the story spec.
 sub pipe_create_story (%args) {
-
-    # This pipe segment doesn't actually use the collection, but builds up the
-    # resulting tree from the story spec.
     return sub ($collection) {
         my @result;
         for my $spec ($args{story}->@*) {
             for my $instruction ($spec->{instructions}->@*) {
                 if ($instruction->{type} eq 'path') {
-                    my $from =
-                      node_for_name($spec->{filename}, $spec->{index}, $instruction->{from});
-                    my $to = node_for_name($spec->{filename}, $spec->{index}, $instruction->{to});
-                    my @nodes_in_path;
-                    my $this_node = $to;
-                    while (1) {
-                        unshift @nodes_in_path, $this_node;
-                        last if $this_node eq $from;    # compare refaddr
-                        our %parent_of_node;
-                        if (defined(my $parent = parent_of_node($this_node))) {
-                            $this_node = $parent;
-                        } else {
-                            die sprintf q!file %s index %s: cannot reach node "%s" from node "%s"\n!,
-                              $spec->{filename}, $spec->{index}, $instruction->{from}, $instruction->{to};
-                        }
-                    }
-                    if (@result) {
-
-                        # $original_node is a node that contains the board position
-                        # at the end of the current result node list.
-                        my $original_node = node_for_name($spec->{filename}, $spec->{index},
-                            $result[-1]->directives->{name});
-
-                        # %board_changes contains AW[], AB[] and AE[]. Apply these
-                        # to the path start node, but clone it because that node
-                        # can be in more than one path.
-                        my %board_changes = get_board_changes($original_node, $nodes_in_path[0]);
-                        $nodes_in_path[0] = $nodes_in_path[0]->public_clone;
-                        while (my ($property, $values) = each %board_changes) {
-                            $nodes_in_path[0]->add($property, $values);
-                        }
-                    }
-                    push @result, @nodes_in_path;
+                    my $node_list = handle_path_instruction($spec, $instruction);
+                    append_node_list($node_list, \@result, $spec);
+                } elsif ($instruction->{type} eq 'node') {
+                    my $node_list = handle_node_instruction($spec, $instruction);
+                    append_node_list($node_list, \@result, $spec);
+                } elsif ($instruction->{type} eq 'traverse') {
+                    handle_traverse_instruction($spec, $instruction, \@result);
                 } else {
                     die sprintf qq!unknown instruction type "%s"\n!, $instruction->{type};
                 }
@@ -93,12 +66,77 @@ sub pipe_create_story (%args) {
     };
 }
 
+# This instruction specifies a linear path from one node to another node.
+sub handle_path_instruction ($spec, $instruction) {
+    my $from =
+      node_for_name($spec->{filename}, $spec->{index}, $instruction->{from});
+    my $to = node_for_name($spec->{filename}, $spec->{index}, $instruction->{to});
+    my @node_list;
+    my $this_node = $to;
+    while (1) {
+        unshift @node_list, $this_node;
+        last if $this_node eq $from;    # compare refaddr
+        if (defined(my $parent = parent_for_node($this_node))) {
+            $this_node = $parent;
+        } else {
+            die sprintf q!file %s index %s: cannot reach node "%s" from node "%s"\n!,
+              $spec->{filename}, $spec->{index}, $instruction->{from}, $instruction->{to};
+        }
+    }
+    return \@node_list;
+}
+
+# This instruction specifies a single node.
+sub handle_node_instruction ($spec, $instruction) {
+
+    # This can be simulated with a "path" instruction that has the same "from"
+    # and "to" node.
+    $instruction->{from} = $instruction->{to} = delete $instruction->{node};
+    return handle_path_instruction($spec, $instruction);
+}
+
+# This instruction specifies a depth-first traversal starting from one node.
+sub handle_traverse_instruction ($spec, $instruction, $result_list) {
+    my $from =
+      node_for_name($spec->{filename}, $spec->{index}, $instruction->{from});
+
+    # Find out which tree the node belongs to.
+    my $tree = tree_for_node($from);
+
+    # Traverse the tree. When you have found the "from" node, build up the node
+    # lists for the story paths.
+    my ($did_see_from_node, %did_take_node, @node_list);
+    $tree->traverse(
+        sub ($node, $context) {
+
+            # Take a node if it is the "from" node or if we have already taken
+            # the parent node - that is, we are within the subtree below the
+            # "from" node.
+            if (($node->directives->{name} // '') eq $instruction->{from}) {
+                $did_see_from_node++;
+                $did_take_node{$node}++;
+                push @node_list, $node;
+            } elsif ($did_take_node{ parent_for_node($node) // '' }) {
+                $did_take_node{$node}++;
+                push @node_list, $node;
+            }
+
+            # If the node is at the end of a variation, add the node list to
+            # the story and reset the node list.
+            if ($context->is_variation_end($node)) {
+                append_node_list(\@node_list, $result_list, $spec);
+                @node_list = ();
+            }
+        }
+    );
+}
+
 sub node_for_name ($filename, $index, $name, $new_value = undef) {
-    our %node_for_name;
+    our %_node_for_name;
     if (defined $new_value) {
-        $node_for_name{$filename}[$index]{$name} = $new_value;
+        $_node_for_name{$filename}[$index]{$name} = $new_value;
     } else {
-        if (defined(my $node = $node_for_name{$filename}[$index]{$name})) {
+        if (defined(my $node = $_node_for_name{$filename}[$index]{$name})) {
             return $node;
         } else {
             die qq!file $filename index $index: no node with name "$name"\n!;
@@ -106,14 +144,25 @@ sub node_for_name ($filename, $index, $name, $new_value = undef) {
     }
 }
 
-sub parent_of_node ($node, $parent = undef) {
-    our %parent_for_node;
+sub parent_for_node ($node, $parent = undef) {
+    our %_parent_for_node;
 
     # the hash keys are refaddrs
     if (defined $parent) {
-        $parent_for_node{$node} = $parent;
+        $_parent_for_node{$node} = $parent;
     } else {
-        return $parent_for_node{$node};
+        return $_parent_for_node{$node};
+    }
+}
+
+sub tree_for_node ($node, $tree = undef) {
+    our %_tree_for_node;
+
+    # the hash keys are refaddrs
+    if (defined $tree) {
+        $_tree_for_node{$node} = $tree;
+    } else {
+        return $_tree_for_node{$node};
     }
 }
 
@@ -142,6 +191,26 @@ sub get_board_changes ($from_node, $to_node) {
         }
     }
     return %board_changes;
+}
+
+sub append_node_list ($node_list, $result_list, $spec) {
+    if ($result_list->@*) {
+
+        # $original_node is a node that contains the board position
+        # at the end of the current result node list.
+        my $original_node = node_for_name($spec->{filename}, $spec->{index},
+            $result_list->[-1]->directives->{name});
+
+        # %board_changes contains AW[], AB[] and AE[]. Apply these
+        # to the path start node, but clone it because that node
+        # can be in more than one path.
+        my %board_changes = get_board_changes($original_node, $node_list->[0]);
+        $node_list->[0] = $node_list->[0]->public_clone;
+        while (my ($property, $values) = each %board_changes) {
+            $node_list->[0]->add($property, $values);
+        }
+    }
+    push $result_list->@*, $node_list->@*;
 }
 1;
 
--- t/porcelain/linear_story/make_linear_story.t
+++ t/porcelain/linear_story/make_linear_story.t
@@ -7,19 +7,30 @@ use Test::More;
 use Test::Differences;
 
 sub porcelain_ok (%args) {
-    my $porcelain = GoGameTools::Porcelain::MakeLinearStory->new(
-        story_file  => $args{story_file},
-        output_file => 'result.sgf'
-    );
-    my ($collection) = run_pipe($porcelain->run);
-    my $expect_sgf = slurp($args{expect_file});
-    eq_or_diff $collection->[0]->as_sgf, $expect_sgf,
-      "$args{name}: linear story SGF";
-    return $collection;
+    subtest $args{name} => sub {
+        my $porcelain = GoGameTools::Porcelain::MakeLinearStory->new(
+            story_file  => $args{story_file},
+            output_file => 'result.sgf'
+        );
+        my ($collection) = run_pipe($porcelain->run);
+        my $expect_sgf = slurp($args{expect_file});
+        eq_or_diff $collection->[0]->as_sgf, $expect_sgf, 'linear story SGF';
+        return $collection;
+    }
 }
+
+# Use the story file that has explicit "path" and "node" instructions.
 porcelain_ok(
-    name        => 'k14-ch02-leaning-01',
-    story_file  => 't/porcelain/linear_story/k14-ch02-leaning-01-story.json',
+    name        => 'k14-ch02-leaning-01 with "path" and "node"',
+    story_file  => 't/porcelain/linear_story/k14-ch02-leaning-01-path-story.json',
+    expect_file => 't/porcelain/linear_story/k14-ch02-leaning-01-expect.sgf'
+);
+
+# Use the story file that has the "traverse" instruction.
+porcelain_ok(
+    name       => 'k14-ch02-leaning-01 with "traverse"',
+    story_file =>
+      't/porcelain/linear_story/k14-ch02-leaning-01-traverse-story.json',
     expect_file => 't/porcelain/linear_story/k14-ch02-leaning-01-expect.sgf'
 );
 done_testing;
